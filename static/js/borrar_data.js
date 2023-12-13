0b68cfba0743c471304c101e747ceea5d4f4b280
AmairaniMartinezM <122801592+AmairaniMartinezM@users.noreply.github.com>
2023-12-12 21:29:12 -0600

    Update borrar_data.js

--- static/js/borrar_data.js
+++ static/js/borrar_data.js
@@ -4,8 +4,8 @@ function borrar_data(email){
     sessionStorage.setItem('token', token);
 
     var email = urlParams.get('email');
-    const url = "http://localhost:8000/contactos";
-    //const URL = "https://nueva-api-344c8029d97b.herokuapp.com/contactos"
+    //const url = "http://localhost:8000/contactos";
+    const URL = "https://nueva-api-344c8029d97b.herokuapp.com/contactos"
     if (token){
         fetch(url, {
             method: 'GET',
@@ -51,7 +51,7 @@ function borrar_data(email){
       })
       .catch(error => {
         alert("No autorizado");
-        //window.location.href = "http://localhost:8080/templates/index.html";
+        //window.location.href = "https://nueva-api-344c8029d97b.herokuapp.com/templates/index.html";
       });
     } else {
       console.log('No se encontró un token.');
