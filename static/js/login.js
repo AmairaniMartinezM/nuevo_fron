d4bfc2b0987e869fe6db422f98ce9c61b720dcc4
AmairaniMartinezM <122801592+AmairaniMartinezM@users.noreply.github.com>
2023-12-12 21:40:07 -0600

    Update login.js

--- static/js/login.js
+++ static/js/login.js
@@ -1,5 +1,5 @@
 function login(username, passwd){
-    endpoint = "http://localhost:8000/token"
+    endpoint = "https://nueva-api-344c8029d97b.herokuapp.com/contactos"
     const token = `${username}:${passwd}`;
     const tokenBase64 = btoa(token);
     fetch(endpoint, {
@@ -18,9 +18,9 @@ function login(username, passwd){
     .then(data => {
         sessionStorage.setItem('token', data.token);
         console.log('Inicio de sesión exitoso. Token:', data.token);
-        window.location.href = "http://localhost:8080/templates/todos.html?token="+data.token;
+        window.location.href = "https://nueva-api-344c8029d97b.herokuapp.com/templates/todos.html?token="+data.token;
         //window.location.href = "http://localhost:8080/templates/todos.html"
-        //window.location.href = "https://frontentapi-de4686146bd2.herokuapp.com/?token="+data.token;
+        //window.location.href = "https://nueva-api-344c8029d97b.herokuapp.com/?token="+data.token;
     })
     .catch(error => {
         alert(error.message);
